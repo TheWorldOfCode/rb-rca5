86dd68a1dec24792f723bf5444f202bad3a68e8c
Lars L <lalin17@student.sdu.dk>
2019-11-15 11:20:37 +0100

    hmm

--- robot_control/src/main.cpp
+++ robot_control/src/main.cpp
@@ -276,7 +276,7 @@ int main(int _argc, char **_argv) {
     float dir = 0.0;
 #endif
 
-    controller.setGoal(-4,-1);
+    controller.setGoal(-5,-3.5);
 
     float marbleDir, marbleDist;
     bool marbleFound;
